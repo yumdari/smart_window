e21422b9db527e4beb9f2946832637d0caad9fb4
epqmfzkdlwj@naver.com <epqmfzkdlwj@naver.com>
2022-05-23 21:24:59 +0900

    raspberry

---
+++ post-esp-data.php
@@ -0,0 +1,59 @@
+<?php
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "esp_data";
+// REPLACE with Database user
+$username = "root";
+// REPLACE with Database user password
+$password = "kcci";
+
+// Keep this API Key value to be compatible with the ESP32 code provided in the>
+// If you change this value, the ESP32 sketch needs to match
+$api_key_value = "tPmAT5Ab3j7F9";
+
+$api_key= $sensor = $location = $temp = $humi = $value3 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_value) {
+        $sensor = test_input($_POST["sensor"]);
+        $location = test_input($_POST["location"]);
+        $temp = test_input($_POST["temp"]);
+        $humi = test_input($_POST["humi"]);
+        $value3 = test_input($_POST["value3"]);
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO SensorData (sensor, location, temp, humi, value>
+        VALUES ('" . $sensor . "', '" . $location . "', '" . $temp . "', '" .>
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        }
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
+    else {
+        echo "Wrong API Key provided.";
+    }
+
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
